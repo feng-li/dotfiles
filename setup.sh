ebb5309d977e45755e62c4508ec4d10227feae7c
Feng Li <m@feng.li>
2020-03-31 09:45:13 +0800

    Update

--- setup.sh
+++ setup.sh
@@ -8,13 +8,14 @@ echo $DIR
 
 rm -rf $HOME/.dotfiles
 ln -sf $DIR $HOME/.dotfiles
-echo dotfiles setup: 'dotfiles' is softlinked to  $HOME/.dotfiles
-
+echo dotfiles setup: $DIR is softlinked to  $HOME/.dotfiles
 
 # Emacs
+mv -f $HOME/.emacs.d  $HOME/.emacs.d_bak
+rm -rf $HOME/.emacs
+
 ln -sf $DIR/.emacs.d $HOME
-mv $HOME/.emacs $HOME/.emacs_bak
-echo dotfiles setup: 'dotfiles/emacs.d/' is softlinked to  $HOME/.emacs.d/
+echo dotfiles setup: $DIR/emacs.d/ is softlinked to  $HOME/.emacs.d/
 
 echo Enabling emacs daemon via systemctl
 systemctl enable --user emacs
@@ -23,28 +24,30 @@ echo Starting emacs daemon via systemctl
 systemctl start --user emacs
 
 ln -sf $DIR/.inputrc $HOME
-echo dotfiles setup: 'dotfiles/.inputrc' is softlinked to  $HOME/.inputrc
+echo dotfiles setup: $DIR/.inputrc is softlinked to  $HOME/.inputrc
 
 ln -sf $DIR/.zshrc $HOME
-echo dotfiles setup: 'dotfiles/.zshrc' is softlinked to  $HOME/.zshrc
+echo dotfiles setup: $DIR/.zshrc is softlinked to  $HOME/.zshrc
 
 ln -sf $DIR/.tmux.conf $HOME
-echo dotfiles setup: 'dotfiles/.tmux.conf' is softlinked to  $HOME/.tmux.conf
+echo dotfiles setup: $DIR/.tmux.conf is softlinked to  $HOME/.tmux.conf
 
 ln -sf $DIR/.gitconfig $HOME
-echo dotfiles setup: 'dotfiles/.gitconfig' is softlinked to  $HOME/.gitconfig
+echo dotfiles setup: $DIR/.gitconfig is softlinked to  $HOME/.gitconfig
 
 ln -sf $DIR/.Renviron $HOME
-echo dotfiles setup: 'dotfiles/.Renviron' is softlinked to  $HOME/.Renviron
+echo dotfiles setup: $DIR/.Renviron is softlinked to  $HOME/.Renviron
 
 ln -sf $DIR/.Rprofile $HOME
-echo dotfiles setup: 'dotfiles/.Rprofile' is softlinked to  $HOME/.Rprofile
+echo dotfiles setup: $DIR/.Rprofile is softlinked to  $HOME/.Rprofile
+
+ln -sf $DIR/.R/Makevars $HOME/.R/
+echo dotfiles setup: $DIR/.R/Makevars is softlinked to  $HOME/.R/Makevars
 
 ln -sf $DIR/.lintr $HOME
-echo dotfiles setup: 'dotfiles/.lintr' is softlinked to  $HOME/.lintr
-
+echo dotfiles setup: $DIR/.lintr is softlinked to  $HOME/.lintr
 
 ln -sf $DIR/flake8 $HOME/.config/flake8
-echo dotfiles setup: 'dotfiles/flake8' is softlinked to  $HOME/.config/flake8
+echo dotfiles setup: $DIR/flake8 is softlinked to  $HOME/.config/flake8
 
 exit 0;
