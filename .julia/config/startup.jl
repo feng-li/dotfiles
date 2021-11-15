3a394918e842d52d9308bd26dabaa935ea6e2059
Feng Li <m@feng.li>
2021-11-15 17:56:46 +0800

    Add Julia mirror

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -3,3 +3,5 @@ ENV["DOCUMENTER_DEBUG"] = "true"
 
 # Julia editor
 ENV["JULIA_EDITOR"] = "emacsclient -nw -c"
+ENV["JULIA_PKG_SERVER"] = "https://mirrors.ustc.edu.cn/julia"
+# ENV["JULIA_PKG_SERVER"] = "https://mirrors.tuna.tsinghua.edu.cn/julia"
