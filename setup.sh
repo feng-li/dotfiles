53cd72161978de20fbaff7f43c7e29b150c55993
Feng Li <m@feng.li>
2021-12-09 16:00:10 +0800

    Arrange structures

--- setup.sh
+++ setup.sh
@@ -1,34 +1,34 @@
 #! /usr/bin/env bash
 
 # Get current dir path for this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "dotfiles locate at $DIR"
+echo  "[`dotfiles` located at $dotfiles_dir]"
+echo  "[`dotfiles` setup:]"
+cd $dotfiles_dir
+# git submodule update --recursive
+# git submodule foreach git checkout master
+# git submodule foreach git pull
 
-cd $DIR
-git submodule update --recursive
-git submodule foreach git checkout master
-git submodule foreach git pull
-
-if [ "$(realpath "$DIR")" = "$(realpath "$HOME/.dotfiles")"  ]; then
-    echo "Location of dotfiles and $HOME/.dotfiles are the same."
+if [ "$(realpath "$dotfiles_dir")" = "$(realpath "$HOME/.dotfiles")"  ]; then
+    echo  "Location of `dotfiles` and $HOME/.dotfiles are the same."
 else
     rm -rf $HOME/.dotfiles
-    ln -sf $DIR $HOME/.dotfiles
-    echo "dotfiles setup: $DIR is softlinked to  $HOME/.dotfiles"
+    ln -sf $dotfiles_dir    $HOME/.dotfiles
+    echo  "$dotfiles_dir -> $HOME/.dotfiles"
 fi
 
 # Emacs
 mv -f $HOME/.emacs.d  $HOME/.emacs.d_bak
 rm -rf $HOME/.emacs
 
-ln -sf $DIR/.emacs.d $HOME
-echo "dotfiles setup: $DIR/emacs.d/ is softlinked to  $HOME/.emacs.d/"
+ln -sf $dotfiles_dir/.emacs.d    $HOME/.emacs.d
+echo  "$dotfiles_dir/emacs.d/ -> $HOME/.emacs.d/"
 
-# echo Enabling emacs daemon via systemctl
+# echo  Enabling emacs daemon via systemctl
 # systemctl enable --user emacs
 
-# echo Starting emacs daemon via systemctl
+# echo  Starting emacs daemon via systemctl
 # systemctl start --user emacs
 
 if [ ! -d $HOME/.local/bin/ ]; then
@@ -36,48 +36,50 @@ if [ ! -d $HOME/.local/bin/ ]; then
 fi
 
 # git-latexdiff
-ln -sf $DIR/git-latexdiff/git-latexdiff $HOME/.local/bin/
-echo "git-latexdiff is softlinked to $HOME/.local/bin/"
+ln -sf $dotfiles_dir/git-latexdiff/git-latexdiff    $HOME/.local/bin/
+echo  "$dotfiles_dir/git-latexdiff/git-latexdiff -> $HOME/.local/bin/"
 
-ln -sf $DIR/.inputrc $HOME
-echo "dotfiles setup: $DIR/.inputrc is softlinked to  $HOME/.inputrc"
+ln -sf $dotfiles_dir/.latexmkrc    $HOME/.latexmkrc
+echo  "$dotfiles_dir/.latexmkrc -> $HOME/.latexmkrc"
 
-ln -sf $DIR/.zshrc $HOME
-echo "dotfiles setup: $DIR/.zshrc is softlinked to  $HOME/.zshrc"
+ln -sf $dotfiles_dir/.inputrc    $HOME/.inputrc
+echo  "$dotfiles_dir/.inputrc -> $HOME/.inputrc"
 
-ln -sf $DIR/.tmux.conf $HOME
-echo "dotfiles setup: $DIR/.tmux.conf is softlinked to  $HOME/.tmux.conf"
+ln -sf $dotfiles_dir/.zshrc    $HOME/.zshrc
+echo  "$dotfiles_dir/.zshrc -> $HOME/.zshrc"
 
-ln -sf $DIR/.gitconfig $HOME
-echo "dotfiles setup: $DIR/.gitconfig is softlinked to  $HOME/.gitconfig"
+ln -sf $dotfiles_dir/.tmux.conf    $HOME/.tmux.conf
+echo  "$dotfiles_dir/.tmux.conf -> $HOME/.tmux.conf"
 
-ln -sf $DIR/.Renviron $HOME
-echo "dotfiles setup: $DIR/.Renviron is softlinked to  $HOME/.Renviron"
+ln -sf $dotfiles_dir/.gitconfig    $HOME/.gitconfig
+echo  "$dotfiles_dir/.gitconfig -> $HOME/.gitconfig"
 
-ln -sf $DIR/.Rprofile $HOME
-echo "dotfiles setup: $DIR/.Rprofile is softlinked to  $HOME/.Rprofile"
+ln -sf $dotfiles_dir/.Renviron    $HOME/.Renviron
+echo  "$dotfiles_dir/.Renviron -> $HOME/.Renviron"
+
+ln -sf $dotfiles_dir/.Rprofile    $HOME/.Rprofile
+echo  "$dotfiles_dir/.Rprofile -> $HOME/.Rprofile"
 
 if [ ! -d $HOME/.R ]; then
     mkdir $HOME/.R
 fi
-ln -sf $DIR/.R/Makevars $HOME/.R/
-echo "dotfiles setup: $DIR/.R/Makevars is softlinked to  $HOME/.R/Makevars"
+ln -sf $dotfiles_dir/.R/Makevars    $HOME/.R/Makevars
+echo  "$dotfiles_dir/.R/Makevars -> $HOME/.R/Makevars"
 
-ln -sf $DIR/.lintr $HOME
-echo "dotfiles setup: $DIR/.lintr is softlinked to  $HOME/.lintr"
+ln -sf $dotfiles_dir/.lintr    $HOME/.lintr
+echo  "$dotfiles_dir/.lintr -> $HOME/.lintr"
 
 if [ ! -d $HOME/.config ]; then
     mkdir $HOME/.config
 fi
 
-ln -sf $DIR/flake8 $HOME/.config/flake8
-echo "dotfiles setup: $DIR/flake8 is softlinked to  $HOME/.config/flake8"
-
+ln -sf $dotfiles_dir/.config/flake8     $HOME/.config/flake8
+echo  "$dotfiles_dir/.config/flake8 ->  $HOME/.config/flake8"
 
 if [ ! -d $HOME/.config/pip ]; then
     mkdir -p $HOME/.config/pip
 fi
-ln -sf $DIR/pip.conf $HOME/.config/pip/pip.conf
-echo "dotfiles setup: $DIR/pip.conf is softlinked to  $HOME/.config/pip/pip.conf"
+ln -sf $dotfiles_dir/.config/pip/pip.conf    $HOME/.config/pip/pip.conf
+echo  "$dotfiles_dir/.config/pip/pip.conf -> $HOME/.config/pip/pip.conf"
 
 exit 0;
