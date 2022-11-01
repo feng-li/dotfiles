524e7e11cf7db441cc6f017029bcd92c629c72ba
Feng Li <m@feng.li>
2022-11-01 15:46:30 +0800

    Update JAVA PATH

--- setvars/spark-standalone.sh
+++ setvars/spark-standalone.sh
@@ -1,3 +1,4 @@
-export JAVA_HOME=/usr/lib/jvm/java
+# export JAVA_HOME=/usr/lib/jvm/java
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.ea.28-7.el7.x86_64
 export SPARK_HOME=$HOME/.local/mapreduce/spark-current
-export PATH=$SPARK_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:$SPARK_HOME/bin:$PATH
