bf74b2e9ffa799142811ecf3033727032cc62dbb
Feng Li <m@feng.li>
2024-04-25 10:01:06 +0800

    Add tex2docx.sh

---
+++ bin/tex2docx.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash -e
+
+tex_input=$1
+
+if [ $tex_input==''  ]; then
+    echo "Input tex file required"
+    exit 1;
+fi
+
+doc_output=${tex_input%%.*}.docx
+
+bib_file=/home/fli/texmf/bibtex/bib/references-feng.bib
+
+pandoc --bibliography=$bib_file --citeproc $tex_input -o $doc_output
