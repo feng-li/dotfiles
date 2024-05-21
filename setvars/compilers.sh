7ed976d2616a22fa8c9d727dd854d1630d8301c6
Feng Li <m@feng.li>
2024-05-21 17:44:37 +0800

    set mkl env

--- setvars/compilers.sh
+++ setvars/compilers.sh
@@ -13,7 +13,7 @@ GCC_HOME=$HOME/.local/compilers/gcc-jit-10.2.0
 CMAKE_HOME=$HOME/.local/compilers/cmake-3.24.2
 LLVM_HOME=$HOME/.local/compilers/llvm-15.0.3
 
-##
+## MKL
 MKLROOT=$HOME/.local/intel/oneapi/mkl/latest
 
 ## CentOS DEVTOOLSET
@@ -22,7 +22,6 @@ DEVTOOLSET=/opt/rh/devtoolset-11
 my_set_dev_env(){
 
     ## GCC
-    # GCC_HOME=$HOME/.local/compilers/gcc-9.5.0
     export PATH=${GCC_HOME}/bin:$PATH
     export LD_LIBRARY_PATH=${GCC_HOME}/lib64:${GCC_HOME}/lib:$LD_LIBRARY_PATH
     export MANPATH=${GCC_HOME}/share/man:$MANPATH
@@ -37,9 +36,18 @@ my_set_dev_env(){
     ## LLVM
     export PATH=$LLVM_HOME/bin:$PATH
 
+}
+
+my_set_mkl_env(){
+
     ## Intel MKL
-    ## export MKL_LIB_PATH=$MKLROOT/lib/intel64
-    ## export LD_LIBRARY_PATH=$MKL_LIB_PATH:$LD_LIBRARY_PATH
+    export MKL_LIB_PATH=$MKLROOT/lib/intel64
+    export LD_LIBRARY_PATH=$MKL_LIB_PATH:$LD_LIBRARY_PATH
+
+    ## dynamic preload to avoid lib conflict
+    export LD_PRELOAD=$MKL_LIB_PATH/libmkl_core.so:$MKL_LIB_PATH/libmkl_sequential.so 
+
+    
 }
 
 my_set_devtoolset_env(){
@@ -59,9 +67,11 @@ my_unset_dev_env(){
     export PKG_CONFIG_PATH=$orig_PKG_CONFIG_PATH
     export C_INCLUDE_PATH=$orig_C_INCLUDE_PATH
     export CPLUS_INCLUDE_PAT=$ORGIG_CPLUS_INCLUDE_PATH
+    unset  LD_PRELOAD
 
 }
 
 # Activate the dev environment
 # my_set_dev_env
 my_set_devtoolset_env
+my_set_mkl_env
